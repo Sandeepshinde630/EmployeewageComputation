9041b16a96f6ba6fb64833a791e36a8cb5205b81
unknown <sandeepshinde630@gmail.com>
2021-07-20 09:48:13 +0530

    Empwage Attendance

--- EmployeewageProblem.sh
+++ EmployeewageProblem.sh
@@ -1,2 +1,11 @@
-#! /bin/bash -x
-echo "welcome to problem"
+#!/bin/bash -x
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is Present";
+else
+        echo "Employee is Absent";
+fi
